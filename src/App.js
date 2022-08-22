92f3f6ca4939997ef7d9f42ea47a49e6c8a9098a
vijaybadi <vijaybadi420@gmail.com>
2022-08-22 23:16:32 +0530

    complete
    complete

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import logo1 from './logo512.png';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo1} className="App-logo" alt="logo" />
+        <p>
+         welcome to reva university
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
